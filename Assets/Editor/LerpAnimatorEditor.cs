f685bf606b24a46d7c6b377bf2ffcecda223e9ce
BenediktOrnHjaltason <46516667+BenediktOrnHjaltason@users.noreply.github.com>
2021-09-21 20:44:16 +0200

    Initial commit

---
+++ Assets/Editor/LerpAnimatorEditor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(LerpAnimator))]
+public class LerpAnimatorEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("TransformsToActOn"), true);
+
+        serializedObject.ApplyModifiedProperties();
+    }
+}
---
+++ Assets/LerpAnimator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class LerpAnimator : MonoBehaviour
+{
+    public class Sequence
+    {
+        //List<Transform>
+    }
+
+    [SerializeField] public Transform[] TransformsToActOn;
+
+    [SerializeField] public Transform[] StartTransforms;
+}
