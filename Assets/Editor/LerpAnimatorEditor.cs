399b29fd6c91429495fc59d5ea4bb2f6af337227
BenediktOrnHjaltason <46516667+BenediktOrnHjaltason@users.noreply.github.com>
2021-09-23 22:23:16 +0200

    Adding and removing segments. Started on run animation logic

--- Assets/Editor/LerpAnimatorEditor.cs
+++ Assets/Editor/LerpAnimatorEditor.cs
@@ -3,15 +3,228 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
+
+
 [CustomEditor(typeof(LerpAnimator))]
 public class LerpAnimatorEditor : Editor
 {
+    LerpAnimator animator;
+
+    private void OnEnable()
+    {
+        animator = (LerpAnimator)target;
+
+        EditorApplication.update += OnEditorUpdate;
+    }
+
+    private void OnDisable()
+    {
+        //ApplyStartState();
+        EditorApplication.update -= OnEditorUpdate;
+    }
+
+    bool justModifiedSegmentsNumer = false;
+
     public override void OnInspectorGUI()
     {
         serializedObject.Update();
 
         EditorGUILayout.PropertyField(serializedObject.FindProperty("TransformsToActOn"), true);
 
+        if (GUILayout.Button("Sample"))
+        {
+            SampleStartState();
+        }
+
+        if (GUILayout.Button("Apply"))
+        {
+            ApplyStartState();
+        }
+
+        if(animator.Segments.Count > 0)
+        {
+            if (GUILayout.Button("Play from start"))
+            {
+                ApplyStartState();
+            }
+        }
+
+
+
+        GUILayout.Label("Segments");
+
+        int numberOfSegments = serializedObject.FindProperty("Segments").arraySize;
+
+        if (GUILayout.Button("Add segment"))
+        {
+            Debug.Log("Pressed Add segment");
+            AddSegment();
+            justModifiedSegmentsNumer = true;
+            
+        }
+
+        if (GUILayout.Button("Remove segment"))
+        {
+            if (numberOfSegments < 2) return;
+
+            Debug.Log("Pressed Remove segment");
+            RemoveSegment();
+            justModifiedSegmentsNumer = true;
+        }
+
+        //EditorGUILayout.PropertyField(serializedObject.FindProperty("Segments"));
+
+        
+        if (!justModifiedSegmentsNumer)
+        {
+            
+
+            for (int i = 0; i < numberOfSegments; i++)
+            {
+                EditorGUILayout.PropertyField(serializedObject.FindProperty("Segments").GetArrayElementAtIndex(i));
+
+                if (GUILayout.Button("Sample"))
+                {
+                }
+
+                if (GUILayout.Button("Apply"))
+                {
+                }
+
+                if (GUILayout.Button("Play from here"))
+                {
+                }
+            }
+
+            
+        }
+        justModifiedSegmentsNumer = false;
+
         serializedObject.ApplyModifiedProperties();
+    }
+
+    private void SampleStartState()
+    {
+        animator.startStates = new Dictionary<Transform,TransformData>();
+
+        foreach(Transform transform in animator.TransformsToActOn)
+        {
+            animator.startStates[transform] = new TransformData(transform.localPosition, transform.localRotation.eulerAngles, transform.localScale);
+        }
+    }
+
+    private void AddSegment()
+    {
+        serializedObject.FindProperty("Segments").arraySize++;
+        serializedObject.ApplyModifiedProperties();
+    }
+
+    private void RemoveSegment()
+    {
+        serializedObject.FindProperty("Segments").arraySize--;
+        serializedObject.ApplyModifiedProperties();
+    }
+
+    private void ApplyStartState(int index = -1)
+    {
+        if (index == -1)
+        {
+            for (int i = 0; i < animator.TransformsToActOn.Count; i++)
+            {
+                animator.TransformsToActOn[i].localPosition = animator.startStates[animator.TransformsToActOn[i]].position;
+                animator.TransformsToActOn[i].localRotation = Quaternion.Euler(animator.startStates[animator.TransformsToActOn[i]].rotation);
+                animator.TransformsToActOn[i].localScale = animator.startStates[animator.TransformsToActOn[i]].scale;
+            }
+        }
+    }
+
+    double startTime;
+    double step;
+    private void StartSequence()
+    {
+        if (animator.Segments.Count == 0) return;
+
+        startTime = EditorApplication.timeSinceStartup;
+    }
+    /*
+    private bool running = false;
+
+    int fromIndex = 0;
+    int toIndex = 0;
+    public void RunSequence(int pFromIndex = -1)
+    {
+        if (animator.Segments.Count == 0) return;
+
+        startTime = EditorApplication.timeSinceStartup;
+
+        //If Starting from start states
+        if (pFromIndex == -1)
+        {
+            fromIndex = 0; toIndex = 0;
+
+            animator.Segments[0].OnSegmentStart?.Invoke();
+
+            while (CalculatingInterpolationStep(startTime, animator.Segments[0].duration, out step))
+            {
+                for (int i = 0; i < animator.TransformsToActOn.Count; i++)
+                {
+                    animator.TransformsToActOn[i].localPosition =
+                        Vector3.LerpUnclamped(animator.startStates[animator.TransformsToActOn[i]].position, animator.Segments[0].toTransformData[animator.TransformsToActOn[i]].position,
+                        animator.Segments[0].curve.Evaluate((float)step));
+
+                    animator.TransformsToActOn[i].localRotation =
+                        Quaternion.Euler(Vector3.LerpUnclamped(animator.startStates[animator.TransformsToActOn[i]].rotation, animator.Segments[0].toTransformData[animator.TransformsToActOn[i]].rotation,
+                        animator.Segments[0].curve.Evaluate((float)step)));
+
+                    animator.TransformsToActOn[i].localScale =
+                        Vector3.LerpUnclamped(animator.startStates[animator.TransformsToActOn[i]].scale, animator.Segments[0].toTransformData[animator.TransformsToActOn[i]].scale,
+                        animator.Segments[0].curve.Evaluate((float)step));
+                }
+
+                yield return null;
+            }
+        }
+        //If starting from any sequence
+        else
+        {
+            if (fromIndex + 2 >= animator.Segments.Count) yield break;
+
+            fromIndex = pFromIndex;
+            toIndex = pFromIndex + 1;
+
+            while (CalculatingInterpolationStep(startTime, animator.Segments[fromIndex].duration, out step))
+            {
+                for (int i = 0; i < animator.TransformsToActOn.Count; i++)
+                {
+                    animator.TransformsToActOn[i].localPosition =
+                        Vector3.LerpUnclamped(animator.Segments[fromIndex].toTransformData[animator.TransformsToActOn[i]].position, animator.Segments[toIndex].toTransformData[animator.TransformsToActOn[i]].position,
+                        animator.Segments[0].curve.Evaluate((float)step));
+
+                    animator.TransformsToActOn[i].localRotation =
+                        Quaternion.Euler(Vector3.LerpUnclamped(animator.Segments[fromIndex].toTransformData[animator.TransformsToActOn[i]].rotation, animator.Segments[toIndex].toTransformData[animator.TransformsToActOn[i]].rotation,
+                        animator.Segments[0].curve.Evaluate((float)step)));
+
+                    animator.TransformsToActOn[i].localScale =
+                        Vector3.LerpUnclamped(animator.Segments[fromIndex].toTransformData[animator.TransformsToActOn[i]].scale, animator.Segments[0].toTransformData[animator.TransformsToActOn[i]].scale,
+                        animator.Segments[0].curve.Evaluate((float)step));
+                }
+
+                yield return null;
+            }
+        }
+    }
+
+    */
+
+    private bool CalculatingInterpolationStep(double startTime, double duration, out double step)
+    {
+        step = (EditorApplication.timeSinceStartup - startTime) / duration;
+
+        return step < 1;
+    }
+
+    private void OnEditorUpdate()
+    {
+
     }
 }
--- Assets/LerpAnimator.cs
+++ Assets/LerpAnimator.cs
@@ -1,16 +1,75 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable]
+public struct TransformData
+{
+    public TransformData(Vector3 pPosition, Vector3 pRotation, Vector3 pScale) { position = pPosition; rotation = pRotation; scale = pScale; }
+
+    public Vector3 position;
+    public Vector3 rotation;
+    public Vector3 scale;
+}
+
+[System.Serializable]
+public class Segment
+{
+    [HideInInspector] public Dictionary<Transform, TransformData> toTransformData;
+
+    public float duration;
+    public AnimationCurve curve;
+
+    public UnityEvent OnSegmentStart;
+}
+
+public enum EEditorOrGame
+{
+    EDITOR,
+    GAME
+}
 
 [System.Serializable]
 public class LerpAnimator : MonoBehaviour
 {
-    public class Sequence
+    EEditorOrGame editorOrGame = EEditorOrGame.EDITOR;
+
+    public List<Transform> TransformsToActOn;
+
+    /// <summary>
+    /// Start states of the chosen transforms
+    /// </summary>
+    public Dictionary<Transform,TransformData> startStates;
+
+    /// <summary>
+    /// Individual segments of complete sequence
+    /// </summary>
+
+    public List<Segment> Segments;
+
+    public UnityAction OnSequenceEnd;
+
+    public void EnsureTransformsAndDataConsistency()
     {
-        //List<Transform>
+        List<Transform> transformsToRemove = new List<Transform>();
+
+        //If data store contains data for removed transform, remove from data store
+        foreach(KeyValuePair<Transform, TransformData> pair in startStates)
+        {
+            if (!TransformsToActOn.Contains(pair.Key)) transformsToRemove.Add(pair.Key);
+        }
+
+        foreach (Transform transform in transformsToRemove)
+        {
+            startStates.Remove(transform);
+            
+            foreach(Segment segment in Segments)
+            {
+                segment.toTransformData.Remove(transform);
+            }
+        }
     }
 
-    [SerializeField] public Transform[] TransformsToActOn;
-
-    [SerializeField] public Transform[] StartTransforms;
+    //EditorApplication.timeSinceStartup
 }
