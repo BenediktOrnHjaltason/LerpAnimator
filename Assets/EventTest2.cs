ec9679188d0b658f1cef2f032fd11f20564357cd
BenediktOrnHjaltason <46516667+BenediktOrnHjaltason@users.noreply.github.com>
2022-07-14 14:41:07 +0200

    Testing events with new system

---
+++ Assets/EventTest2.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventTest2 : MonoBehaviour
+{
+    [SerializeField] List<MeshRenderer> renderers; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TurnOnText()
+    {
+        foreach (var renderer in renderers)
+        {
+            renderer.enabled = true;
+        }
+    }
+}
